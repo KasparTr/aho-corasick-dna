407ed809d24aa1eb320c3a7051b7b50595252190
Kaspar Triebstok <kaspar@nordic.id>
2018-07-20 10:07:08 +0700

    Initial push

---
+++ src/main/java/DNAHealth.java
@@ -0,0 +1,14 @@
+public class DNAHealth {
+    public int highestStrandScore;
+    public int lowestStrandScore;
+
+    public DNAHealth(int lowest, int highest){
+        this.lowestStrandScore = lowest;
+        this.highestStrandScore = highest;
+    }
+
+    @Override
+    public String toString(){
+        return lowestStrandScore+" "+highestStrandScore;
+    }
+}
---
+++ src/main/java/Strand.java
@@ -0,0 +1,11 @@
+public class Strand {
+    public int healthyGeneStartIndex;
+    public int healthyGeneEndIndex;
+    public String geneStrand;
+
+    public Strand(int startIdx, int endIdx, String geneStrand){
+        this.healthyGeneStartIndex = startIdx;
+        this.healthyGeneEndIndex = endIdx;
+        this.geneStrand = geneStrand;
+    }
+}
